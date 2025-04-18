9bd67c4ff4d2dceaca323facffce82ec01b4a9df
Yohan Vallier <162353277+YohanVallier@users.noreply.github.com>
2025-04-18 18:17:13 -0300

    Alteração
    refiz a página de contatos, alterei o menu, melhorei a página de novidades

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,19 @@
+let menuVisivel = false;
+
 function acaoBotao() {
     const menu = document.querySelector("#menu");
     const botao = document.querySelector("#menu-botao");
+    const logo = document.querySelector(".logo")
 
-    if(acaoBotao() == 1) {
-        menu.style.display = "block";
-    }
-    else {
+    if (!menuVisivel) {
+        menu.style.display = "flex";
+        menu.style.flexDirection = "column";
+        menu.style.gap = '10px';
+        logo.style.paddingLeft = "20%";
+        menuVisivel = true;
+    } else {
         menu.style.display = "none";
+        logo.style.paddingLeft = "20%";
+        menuVisivel = false;
     }
 }
