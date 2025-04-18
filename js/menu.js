bd085dbfde9e803fcf4b1a0578671ea37fa4301f
Yohan Vallier <162353277+YohanVallier@users.noreply.github.com>
2025-04-18 20:52:28 -0300

    alteração
    O menu volta ao normal ao sair do modo celular

--- js/menu.js
+++ js/menu.js
@@ -17,3 +17,16 @@ function acaoBotao() {
         menuVisivel = false;
     }
 }
+
+window.addEventListener("resize", function () {
+    const menu = document.querySelector("#menu");
+    const logo = document.querySelector(".logo");
+
+    if (window.innerWidth > 768) {
+        menu.style.display = "flex";
+        menu.style.flexDirection = "row";
+        menu.style.gap = "";
+        logo.style.paddingLeft = "";
+        menuVisivel = false;
+    }
+});
