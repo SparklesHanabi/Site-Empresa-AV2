26d9e5b68237b5c6098c5803e67454ce639c6c9d
Guilherme Koplin <160797338+SparklesHanabi@users.noreply.github.com>
2025-04-24 15:23:09 -0300

    mudança de comentarios e tirar um "s"

--- gravar.php
+++ gravar.php
@@ -3,30 +3,29 @@
 // conexão ao MYSQL
 require 'conexao.php';
 
-// RECEBE OS DADOS DA PAGINA DE CONTATOS
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $mensagem = $_POST['mensagem'];
 
 // Salva no arquivo
-$linha = fopen("catalogos.txt", "a");
+$linha = fopen("catalogo.txt", "a");
 fwrite($linha, "Nome: $nome\nEmail: $email\nMensagem: $mensagem\n---\n");
 fclose($linha);
 
-// Query para o banco
+// bota os dados no banco
 $sql = "INSERT INTO formulario (nome, email, mensagem) VALUES ('$nome', '$email', '$mensagem')";
 
-// Executa a query
+// Executa o comando
 if ($conn->query($sql) === TRUE) {
     // OK!
 } else {
-    // Se der erro, mostra na tela
+    // mostra na tela se der erro
     echo "Erro ao salvar no banco: " . $conn->error;
 }
 
 $conn->close();
 
-// Redireciona de volta
+// mantem na tela de contato
 header("Location: contact.html");
 exit;
 ?>
